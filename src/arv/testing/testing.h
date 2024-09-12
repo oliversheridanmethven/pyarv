a16490937ba28d5bdc4877773343f6014eb61365
Dr Oliver Sheridan-Methven <oliver.sheridan-methven@hotmail.co.uk>
2024-09-12 17:29:14 +0100

    tracking testing.

---
+++ src/arv/testing/testing.h
@@ -0,0 +1,16 @@
+#ifndef TESTING_H_
+#define TESTING_H_
+
+#include <criterion/criterion.h>
+#include <criterion/new/assert.h>
+#include <criterion/redirect.h>
+
+void redirect_all_stdout(void)
+{
+    cr_redirect_stdout();
+    setbuf(stdout, NULL);
+    cr_redirect_stderr();
+    setbuf(stderr, NULL);
+}
+
+#endif /*TESTING_H_*/
